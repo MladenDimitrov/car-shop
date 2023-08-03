10f9b4a5b7da802df3b42e2b7072ac10395602d2
MladenDimitrov <lord56@abv.bg>
2023-08-03 23:15:24 +0300

    started to make the frond end for the products page

--- car_shop/products/urls.py
+++ car_shop/products/urls.py
@@ -1,3 +1,6 @@
+from car_shop.products import views
 from django.urls import path
 
-urlpatterns = []
+urlpatterns = [
+    path('', views.show_products, name='products_page')
+]
--- car_shop/products/views.py
+++ car_shop/products/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
-
+from car_shop.products.models import DriveTrainSemiAxleBox
 # Create your views here.
+
+
+def show_products(request):
+    drivetrain = DriveTrainSemiAxleBox.objects.all()
+    context = {
+        'drivetrain': drivetrain
+    }
+    return render(request, template_name='products_page/catalog.html', context=context)
